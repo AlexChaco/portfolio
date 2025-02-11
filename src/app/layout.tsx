570adf68e5c8652ec79096b3795162d6cb217af2
AlexChaco <axchacoh@gmail.com>
2025-02-10 19:49:03 -0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inconsolata } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+// Cargar la fuente correctamente con `subsets`
+const inconsolata = Inconsolata({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: "--font-inconsolata-sans",
 });
 
 export const metadata: Metadata = {
@@ -19,16 +15,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={inconsolata.variable}>
+      <body className="antialiased">{children}</body>
     </html>
   );
 }
